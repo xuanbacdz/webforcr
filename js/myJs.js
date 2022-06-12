719aa3fb4a60cb5abe2808b51bcc2e40f4a544d1
nguyenxuanbac <96097779+xuanbacdz@users.noreply.github.com>
2022-06-12 09:17:22 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,8 +1,8 @@
 const textConfig = {
   text1: "He luu cậu!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
+  text3: "Cậu thích tớ có phải không nào ._.",
+  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm người iu tớ rùi đó nha :v",
   text5: "Cậu mơ à???",
   text6: "Yêu ơi là yêu <3",
   text7: "lí do cậu thích tớ đi :vvvv",
